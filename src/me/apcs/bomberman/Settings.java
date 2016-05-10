1dab17014a0031c46a81bbb22cc7fdb2bae30372
Preston <prestonpetrie@gmail.com>
2016-05-10 13:12:00 -0500

    Added file logic

--- src/me/apcs/bomberman/Settings.java
+++ src/me/apcs/bomberman/Settings.java
@@ -2,12 +2,16 @@ package me.apcs.bomberman;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Settings {
 	
 	public static Properties p = new Properties();
+	public static boolean readSuccessful = true;
+	public static long lastReadWrite = 0l;
+	
 	private static String file = "./options.properties";
 	private static File f;
 	
@@ -17,15 +21,31 @@ public class Settings {
 	}
 	
 	public static void init() {
+		lastReadWrite = System.currentTimeMillis();
 		f = new File(file);
 		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		if(!f.exists()) {
+			try {
+				f.createNewFile();
+				fos = new FileOutputStream(f);
+				writeProperties(fos);
+			} catch(IOException ioe) {
+				readSuccessful = false;
+			}
+		}
 		try {
-			fis = new FileInputStream(file);
+			fis = new FileInputStream(f);
 			p.load(fis);
 		} catch(IOException ioe) {
-			
+			readSuccessful = false;
 		}
 		
+	}
+	
+	public static void writeProperties(FileOutputStream fos) throws IOException {
+		p.store(fos, null);
+		lastReadWrite = System.currentTimeMillis();
 	}
 	
 }
