6f8edf1e891e7ae14837872d91b1aac97ba42196
NoahSebenste <nsebenste@gmail.com>
2016-05-10 10:24:37 -0500

    added upgradespeed

--- src/me/apcs/bomberman/Bomberman.java
+++ src/me/apcs/bomberman/Bomberman.java
@@ -5,10 +5,16 @@ public class Bomberman extends Inhabitant{
 	private int speed;
 	private int bombCapacity;
 	
+	
 	public Bomberman(int s, int bc)
 	{
 		speed = s;
 		bombCapacity = bc;
+	}
+	
+	public void upgradeSpeed(int amt)
+	{
+		speed += amt;
 	}
 	
 }
