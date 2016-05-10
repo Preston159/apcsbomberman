7e5bbcdde14a3bac7c9a92c136f62a649b7e6eff
Logan Kulinski <Lbkulinski@icloud.com>
2016-05-10 12:22:24 -0500

    Field and constructors added
    I added a two-dimensional array field, a default constructor, and a
    parameterized constructor

--- src/me/apcs/bomberman/Grid.java
+++ src/me/apcs/bomberman/Grid.java
@@ -1,5 +1,35 @@
+/**
+ * Grid.java
+ * @author Logan Kulinski
+ * @author Rayyan Karim
+ * @author Ferris
+ */
+
 package me.apcs.bomberman;
 
 public class Grid<T> {
-
+	//declare field
+	private Object[][] tArr;
+	
+	public Grid() {
+		//initialize tArr
+		tArr = new Object[10][10];
+	} //Grid
+	
+	public Grid(int rows, int cols) {
+		//determine if rows is valid
+		if (rows <= 0) {
+			//throw IllegalArgumentException
+			throw new IllegalArgumentException("rows <= 0");
+		} //end if
+		
+		//determine if cols is valid
+		if (cols <= 0) {
+			//throw IllegalArgumentException
+			throw new IllegalArgumentException("cols <= 0");
+		} //end if
+		
+		//initialize field
+		tArr = new Object[rows][cols];
+	} //Grid
 }
