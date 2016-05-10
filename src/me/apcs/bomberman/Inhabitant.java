2bd6f24e2d6767da13acae4313de6238a9e3de22
Preston <prestonpetrie@gmail.com>
2016-05-10 10:09:05 -0500

    Created method layout

--- src/me/apcs/bomberman/Inhabitant.java
+++ src/me/apcs/bomberman/Inhabitant.java
@@ -1,5 +1,30 @@
 package me.apcs.bomberman;
 
-public abstract class Inhabitant {
+import java.awt.Color;
 
+public abstract class Inhabitant {
+	
+	private Color color;
+	private Location l;
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setColor(Color c) {
+		color = c;
+	}
+	
+	public Location getLocation() {
+		return l;
+	}
+	
+	public void setLocation() {
+		
+	}
+	
+	public void move(int dx, int dy) {
+		
+	}
+	
 }
