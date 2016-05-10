5917345cbfb41b406b4f4198f0a7048decc64562
Preston <prestonpetrie@gmail.com>
2016-05-10 10:20:23 -0500

    Added 'move' method

--- src/me/apcs/bomberman/Inhabitant.java
+++ src/me/apcs/bomberman/Inhabitant.java
@@ -2,7 +2,7 @@ package me.apcs.bomberman;
 
 import java.awt.Color;
 
-public abstract class Inhabitant {
+public class Inhabitant {
 	
 	private Color color;
 	private Location loc;
@@ -20,11 +20,21 @@ public abstract class Inhabitant {
 	}
 	
 	public void setLocation(Location l) {
-		
+		loc = l;
 	}
 	
-	public void move(int dx, int dy) {
-		
+	/**
+	 * Moves <code>Inhabitant</code> by the given distance
+	 * @param dx	Change in x
+	 * @param dy	Change in y
+	 * @return	True if successfully moved, false if not
+	 */
+	public boolean move(int dx, int dy) {
+		Location l = getLocation();
+		l.add(dx, dy);
+		//check isn't out of bounds
+		setLocation(l);
+		return true;
 	}
 	
 }
