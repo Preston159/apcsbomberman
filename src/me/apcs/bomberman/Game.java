9da59fe795129e2bf098d2b555d8d72b1bc60c58
Preston <prestonpetrie@gmail.com>
2016-05-12 10:24:05 -0500

    Made non-static added fields

--- src/me/apcs/bomberman/Game.java
+++ src/me/apcs/bomberman/Game.java
@@ -1,19 +1,35 @@
 package me.apcs.bomberman;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 	
-	private static Grid<Inhabitant> grid;
+	private Grid<Inhabitant> grid;
+	private List<Bomberman> players;
 	
-	public static void main(String[] args) {
+	public void init() {
 		Settings.init();
+		int sizeX = Integer.valueOf(Settings.p.getProperty("gridSizeX"));
+		int sizeY = Integer.valueOf(Settings.p.getProperty("gridSizeY"));
+		grid = new Grid<Inhabitant>(sizeX, sizeY);
+		players = new ArrayList<Bomberman>();
 	}
 	
 	/**
 	 * The <code>Grid</code> of the current game
 	 * @return	The <code>Grid<Inhabitant></code>of the current game
 	 */
-	public static Grid<Inhabitant> getGrid() {
+	public Grid<Inhabitant> getGrid() {
 		return grid;
+	}
+	
+	/**
+	 * A <code>List</code> of all current <code>Bomberman</code>s
+	 * @return	</code>ArrayList<Bomberman></code> containing all <code>Bomberman</code>s on the <code>Grid</code>
+	 */
+	public List<Bomberman> getPlayers() {
+		return players;
 	}
 	
 }
