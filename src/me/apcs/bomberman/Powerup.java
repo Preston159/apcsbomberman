f514ac557638954c09b8f736a7903e64d83be0d5
Zack Seglier <zseliger@gmail.com>
2016-05-12 10:06:41 -0500

    re-wrote Powerup class

--- src/me/apcs/bomberman/Powerup.java
+++ src/me/apcs/bomberman/Powerup.java
@@ -1,8 +1,28 @@
 package me.apcs.bomberman;
 
-public interface Powerup {
-	double duration;
+public class Powerup {
+	public enum Type {SPEED, CAPACITY, CAN_KICK, BOMB_SIZE};
+	private Bomberman bomber;
+	Type type = Type.SPEED;
 	
-	public void onStart();
-	public void onEnd();
+	/**
+	 * upgrades the bomber based on <code>type</code>
+	 */
+	public void powerUp() {
+		if (type == Type.SPEED)
+			bomber.changeSpeed(5);
+		else if (type == Type.CAPACITY)
+			bomber.increaseBombs(1);
+		else if (type == Type.CAN_KICK)
+			bomber.canKick(true);
+		//else if (type == Type.BOMB_SIZE)
+			
+	}
+	
+	public void setBomber(Bomberman b) {
+		bomber = b;
+	}
+	public void setPowerUp(Type t) {
+		type = t;
+	}
 }
