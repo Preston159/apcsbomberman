8cdb89e497516c54e338f541892911f2fce82dc5
Lbkulinski <Lbkulinski@icloud.com>
2016-05-12 10:13:03 -0500

    Added classes for Menu and Gameplay

---
+++ src/me/apcs/bomberman/GameJFrame.java
@@ -0,0 +1,46 @@
+package me.apcs.bomberman;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GameJFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 125860129300201340L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameJFrame frame = new GameJFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GameJFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
