2d105b0fbdd030fb7d84cae793c7cbc9fd976f2d
Preston <prestonpetrie@gmail.com>
2016-05-13 16:45:49 -0500

    Began Explosion class

--- src/me/apcs/bomberman/Explosion.java
+++ src/me/apcs/bomberman/Explosion.java
@@ -1,5 +1,29 @@
+/**
+ * @author Preston Petrie
+ */
+
 package me.apcs.bomberman;
 
+import java.awt.Color;
+
 public class Explosion extends Inhabitant {
+	
+	private int state = 0;
+	
+	public Explosion(Color c, Location l) {
+		super(c, l);
+	}
+	
+	/**
+	 * Increment and get the current state of the explosion
+	 * @return	An integer 0-9 which represents the current frame of the explosion animation or -1 if the explosion animation has ended
+	 */
+	public int incrementState() {
+		if(state >= 9) {
+			super.destroy();
+			return -1;
+		}
+		return state++;
+	}
 	
 }
