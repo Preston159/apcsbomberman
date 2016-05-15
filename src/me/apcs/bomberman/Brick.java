af08fd78ad7fc838a7265ada92d9552723b50eb8
NoahSebenste <nsebenste@gmail.com>
2016-05-15 16:28:39 -0500

    added javadocs

--- src/me/apcs/bomberman/Brick.java
+++ src/me/apcs/bomberman/Brick.java
@@ -6,12 +6,22 @@ public class Brick extends Inhabitant
 {
 	private boolean breakable;
 	
+	/**
+	 * initializes the brick
+	 * @param c
+	 * @param l
+	 * @param b
+	 */
 	public Brick(Color c, Location l, boolean b)
 	{
 		super(c, l);
 		breakable = b;
 	}
 	
+	/**
+	 * returns if the brick is breakable or not
+	 * @return
+	 */
 	public boolean isBreakable()
 	{
 		return breakable; 
