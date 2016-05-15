515a005371a3ab15cd818041919a52f0f735d955
SuperMechaDeathChrist2000bcVersion4Beta <donovanewing1998@gmail.com>
2016-05-15 16:50:13 -0500

    Commented

--- src/me/apcs/bomberman/Bomb.java
+++ src/me/apcs/bomberman/Bomb.java
@@ -31,8 +31,10 @@ public class Bomb extends Inhabitant
 	 */
 	public void explode()
 	{
+		//sets l to bomb location
 		Location l = this.getLocation();
 		
+		//initiate for loop, while x <= Bomb Size
 		for(int x = 0; x <= this.getBombLength(); x++)
 		{
 			boolean right = true;
@@ -40,49 +42,66 @@ public class Bomb extends Inhabitant
 			boolean up = true;
 			boolean down = true;
 			
+			//if you haven't hit a wall, right is true
 			if (right == true)
 			{	
+				//sets l x right of the location of the bomb
 				l = new Location(this.getLocation().getIntX() + x, this.getLocation().getIntY());
+				//if l is a valid location on the grid
 				if (Game.getGrid().isValid(l))
-				{
+				{	
+					//if the location l is a brick, set right to false
 					if(Game.getGrid().get(l) instanceof Brick)
 					{
 						right = false;
 						Brick b = (Brick)Game.getGrid().get(l);
+						//if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
 							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
 					}
+					//if it isn't a brick, set space to explosion
 					else
 						Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
 			}
 			
+			//if you haven't hit a wall yet, left is true
 			if(left == true)
 			{
+				//sets l x spaces left of the bomb placement
 				l = new Location(this.getLocation().getIntX() - x, this.getLocation().getIntY());
+				//if location l is a valid location on the grid
 				if (Game.getGrid().isValid(l))
 				{
+					//if l is a brick, set left to false
 					if(Game.getGrid().get(l) instanceof Brick)
 					{
 						left = false;
 						Brick b = (Brick)Game.getGrid().get(l);
+						//if the brick is breakable, set the location of the brick to an explosion
 						if (b.isBreakable())
 							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 					}
+					//if it isn't a brick, set space to explosion
 					else
 						Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
 			}
 			
+			//if you haven't hit a wall yet, up is true
 			if (up == true)
 			{
+				//sets l x spaces left of the bomb placement
 				l = new Location(this.getLocation().getIntX(), this.getLocation().getIntY() + x);
+				//if location l is a valid location on the grid
 				if (Game.getGrid().isValid(l))
 				{
+					//if l is a brick, set up to false
 					if(Game.getGrid().get(l) instanceof Brick)
 					{
 						up = false;
 						Brick b = (Brick)Game.getGrid().get(l);
+						//if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
 							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 					}
@@ -91,23 +110,29 @@ public class Bomb extends Inhabitant
 				}
 			}
 			
+			//if you haven't hit a wall yet, down is true
 			if (down == true)
 			{
+				//sets l x spaces left of the bomb placement
 				l = new Location(this.getLocation().getIntX(), this.getLocation().getIntY() - x);
+				//if location l is a valid location on the grid
 				if(Game.getGrid().isValid(l))
 				{
+					//if l is a brick, set down to false
 					if(Game.getGrid().get(l) instanceof Brick)
 					{
 						down = false;
 						Brick b = (Brick)Game.getGrid().get(l);
+						//if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
 							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
 					}
+					//if it isn't a brick, set space to explosion
 					else
 						Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
 			}
-			
+			//destroy the bomb
 			this.destroy();
 		}
 	}
