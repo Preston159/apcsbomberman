d886f027e6c21bb3ae26c32a89648dfcbdae72ac
SuperMechaDeathChrist2000bcVersion4Beta <donovanewing1998@gmail.com>
2016-05-15 12:25:01 -0500

    Added is destructible method

--- src/me/apcs/bomberman/Brick.java
+++ src/me/apcs/bomberman/Brick.java
@@ -4,8 +4,16 @@ import java.awt.Color;
 
 public class Brick extends Inhabitant
 {
-	public Brick(Color c, Location l)
+	private boolean destroyable;
+	
+	public Brick(Color c, Location l, boolean d)
 	{
 		super(c, l);
+		destroyable = d;
+	}
+	
+	public boolean isDestroyable()
+	{
+		return destroyable; 
 	}
 }
