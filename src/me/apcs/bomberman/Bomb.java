5ae081608e1f8e879194dfbc5edd1bc2d6563ff4
SuperMechaDeathChrist2000bcVersion4Beta <donovanewing1998@gmail.com>
2016-05-18 09:37:31 -0500

    Made work with array lists
    i really like stuffed crust

--- src/me/apcs/bomberman/Bomb.java
+++ src/me/apcs/bomberman/Bomb.java
@@ -39,6 +39,9 @@ public class Bomb extends Inhabitant
 		boolean left = true;
 		boolean up = true;
 		boolean down = true;
+		boolean c = false;
+		int nn = 0;
+		
 		
 		//initiate for loop, while x <= Bomb Size
 		for(int x = 0; x <= this.getBombLength(); x++)
@@ -54,89 +57,134 @@ public class Bomb extends Inhabitant
 				if (Game.getGrid().isValid(l))
 				{	
 					//if the location l is a brick, set right to false
-					if(Game.getGrid().get(l) instanceof Brick)
+					for (int n = 0; n < Game.getGrid().get(l).size(); n++)
+					{
+						if(Game.getGrid().get(l).get(n) instanceof Brick)
+						{
+							c = true;
+							nn = n;
+						}
+					}
+					
+					if (c == true)
 					{
 						right = false;
-						Brick b = (Brick)Game.getGrid().get(l);
-						//if the brick is breakable, set the location of the brick to an explosion
+						Brick b = (Brick)Game.getGrid().get(l).get(nn);
+						//	if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
 							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
+						//if it isn't a brick, set space to explosion
 					}
-					//if it isn't a brick, set space to explosion
 					else
-						Game.getGrid().set(l, new Explosion(super.getColor(), l));
+							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
+				nn = 0;
+				c = false;
 			}
 			
-			//if you haven't hit a wall yet, left is true
-			if(left == true)
-			{
-				//sets l x spaces left of the bomb placement
+			//if you haven't hit a wall, left is true
+			if (left == true)
+			{	
+				//sets l x right of the location of the bomb
 				l = new Location(this.getLocation().getIntX() - x, this.getLocation().getIntY());
-				//if location l is a valid location on the grid
+				//if l is a valid location on the grid
 				if (Game.getGrid().isValid(l))
-				{
-					//if l is a brick, set left to false
-					if(Game.getGrid().get(l) instanceof Brick)
+				{	
+					//if the location l is a brick, set right to false
+					for (int n = 0; n < Game.getGrid().get(l).size(); n++)
 					{
-						left = false;
-						Brick b = (Brick)Game.getGrid().get(l);
-						//if the brick is breakable, set the location of the brick to an explosion
-						if (b.isBreakable())
-							Game.getGrid().set(l, new Explosion(super.getColor(), l));
+						if(Game.getGrid().get(l).get(n) instanceof Brick)
+						{
+							c = true;
+							nn = n;
+						}
 					}
-					//if it isn't a brick, set space to explosion
+					
+					if (c == true)
+					{
+						right = false;
+						Brick b = (Brick)Game.getGrid().get(l).get(nn);
+						//	if the brick is breakable, set the location of the brick to an explosion
+						if(b.isBreakable())
+							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
+						//if it isn't a brick, set space to explosion
+					}
 					else
-						Game.getGrid().set(l, new Explosion(super.getColor(), l));
+							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
+				nn = 0;
+				c = false;
 			}
 			
 			//if you haven't hit a wall yet, up is true
 			if (up == true)
-			{
-				//sets l x spaces left of the bomb placement
+			{	
+				//sets l x right of the location of the bomb
 				l = new Location(this.getLocation().getIntX(), this.getLocation().getIntY() + x);
-				//if location l is a valid location on the grid
+				//if l is a valid location on the grid
 				if (Game.getGrid().isValid(l))
-				{
-					//if l is a brick, set up to false
-					if(Game.getGrid().get(l) instanceof Brick)
+				{	
+					//if the location l is a brick, set right to false
+					for (int n = 0; n < Game.getGrid().get(l).size(); n++)
 					{
-						up = false;
-						Brick b = (Brick)Game.getGrid().get(l);
-						//if the brick is breakable, set the location of the brick to an explosion
+						if(Game.getGrid().get(l).get(n) instanceof Brick)
+						{
+							c = true;
+							nn = n;
+						}
+					}
+					
+					if (c == true)
+					{
+						right = false;
+						Brick b = (Brick)Game.getGrid().get(l).get(nn);
+						//	if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
-							Game.getGrid().set(l, new Explosion(super.getColor(), l));
+							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
+						//if it isn't a brick, set space to explosion
 					}
 					else
-						Game.getGrid().set(l, new Explosion(super.getColor(), l));
+							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
+				nn = 0;
+				c = false;
 			}
 			
 			//if you haven't hit a wall yet, down is true
 			if (down == true)
-			{
-				//sets l x spaces left of the bomb placement
+			{	
+				//sets l x right of the location of the bomb
 				l = new Location(this.getLocation().getIntX(), this.getLocation().getIntY() - x);
-				//if location l is a valid location on the grid
-				if(Game.getGrid().isValid(l))
-				{
-					//if l is a brick, set down to false
-					if(Game.getGrid().get(l) instanceof Brick)
+				//if l is a valid location on the grid
+				if (Game.getGrid().isValid(l))
+				{	
+					//if the location l is a brick, set right to false
+					for (int n = 0; n < Game.getGrid().get(l).size(); n++)
 					{
-						down = false;
-						Brick b = (Brick)Game.getGrid().get(l);
-						//if the brick is breakable, set the location of the brick to an explosion
+						if(Game.getGrid().get(l).get(n) instanceof Brick)
+						{
+							c = true;
+							nn = n;
+						}
+					}
+					
+					if (c == true)
+					{
+						right = false;
+						Brick b = (Brick)Game.getGrid().get(l).get(nn);
+						//	if the brick is breakable, set the location of the brick to an explosion
 						if(b.isBreakable())
 							Game.getGrid().set(l,  new Explosion(super.getColor(), l));
+						//if it isn't a brick, set space to explosion
 					}
-					//if it isn't a brick, set space to explosion
 					else
-						Game.getGrid().set(l, new Explosion(super.getColor(), l));
+							Game.getGrid().set(l, new Explosion(super.getColor(), l));
 				}
+				nn = 0;
+				c = false;
 			}
-			//destroy the bomb
-			this.destroy();
 		}
+		//destroy the bomb
+		this.destroy();
 	}
 }
