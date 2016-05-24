546143154b2e2b05dea567e5a6a50a751bfbd771
Preston <prestonpetrie@gmail.com>
2016-05-23 22:17:06 -0500

    Fixed possibly destroying self multiple times

--- src/me/apcs/bomberman/Explosion.java
+++ src/me/apcs/bomberman/Explosion.java
@@ -19,7 +19,7 @@ public class Explosion extends Inhabitant {
 	 * @return	An integer 0-9 which represents the current frame of the explosion animation or -1 if the explosion animation has ended
 	 */
 	public int incrementState() {
-		if(state >= 9) {
+		if(state == 9) {
 			super.destroy();
 			return -1;
 		}
