ad1f3224b967b61bab46ac747e8ed830dd709303
Preston <prestonpetrie@gmail.com>
2016-05-24 07:50:09 -0500

    Redid Brick generation

--- src/me/apcs/bomberman/GameDebug.java
+++ src/me/apcs/bomberman/GameDebug.java
@@ -28,6 +28,9 @@ public class GameDebug extends JPanel {
 	private double scaleX, scaleY;
 	private Keyboard keys;
 	
+	private static int gridSizeX;
+	private static int gridSizeY;
+	
 	public GameDebug() {
 		//set focus
 		setFocusable(true);
@@ -36,21 +39,26 @@ public class GameDebug extends JPanel {
 		//initialize settings
 		Settings.init();
 		tickLength = (int) ((double) 1 / Integer.valueOf(Settings.p.getProperty("ticksPerSecond")) * 1000);
-		int gridSizeX = Integer.valueOf(Settings.p.getProperty("gridSizeX"));
-		int gridSizeY = Integer.valueOf(Settings.p.getProperty("gridSizeY"));
+		gridSizeX = Integer.valueOf(Settings.p.getProperty("gridSizeX"));
+		gridSizeY = Integer.valueOf(Settings.p.getProperty("gridSizeY"));
 		scaleX = WIDTH / gridSizeX;
 		scaleY = HEIGHT / gridSizeY;
 		Game.createGrid();
 		players = new ArrayList<Bomberman>();
 		
 		//add one Inhabitant
-		players.add(new Bomberman(Color.BLUE, new Location(0,0), 0.1, 1, 2));
+		players.add(new Bomberman(Color.BLUE, new Location(1,1), 0.1, 1, 2));
 		Game.queueAdd(players.get(0));
 		
 		//add some Bricks
-		for (int i = 0; i < 7; i++) {
+		ArrayList<Brick> bricks = genBricks();
+	
+		for(Brick b : bricks)
+			Game.queueAdd(b);
+		
+	/*	for (int i = 0; i < 7; i++) {
 			Game.queueAdd(new Brick(Color.GRAY, new Location((int)(Math.random() * gridSizeX), (int)(Math.random() * gridSizeY)), false, (ArrayList<Bomberman>) players));
-		}
+		}	*/
 		
 		//set up keyboard actions
 		keys = new Keyboard();
@@ -101,6 +109,53 @@ public class GameDebug extends JPanel {
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    w.setResizable(false);
 	    w.setVisible(true);
+	}
+	
+	private static ArrayList<Brick> genBricks() {
+		ArrayList<Brick> bricks = new ArrayList<Brick>();
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(1, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(2, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(3, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(4, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(5, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(6, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(7, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(8, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 0), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(1, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(2, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(3, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(4, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(5, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(6, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(7, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(8, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 9), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 1), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 1), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 2), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 2), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 3), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 3), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 4), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 4), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 5), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 5), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 6), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 6), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 7), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 7), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(0, 8), false));
+		bricks.add(new Brick(Color.DARK_GRAY, new Location(9, 8), false));
+		for(int i = 0;i < 8;i++) {
+			Location l = new Location((int) (Math.random() * gridSizeX), (int) (Math.random() * gridSizeY));
+			if(!Game.getGrid().isEmpty(l))
+				continue;
+			bricks.add(new Brick(Color.GRAY, l, true));
+		}
+		return bricks;
 	}
 }
 
